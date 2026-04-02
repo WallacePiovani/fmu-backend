9cdf4a31f7d4de6a3c4b2ed76c8f0f38f3ab8431
Wallace Piovani <whspiovani@gmail.com>
2026-04-02 10:57:27 -0300

    Feito alguns testes utilizando express para fazer um sistema de rotas

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import express from 'express';
-
+import path from 'path';
 
 const app = express ();
-const port = 8080;
 
-app.get ('/', (req,res) => {
-    res.send ('<h1>Olá Mundo!</h1>')
+app.get('/', (req, res) => {
+    // res.send -> manda uma resposta em texto  
+    res.sendFile(path.resolve('./src/index.html'));
+    // path.resolve -> transforma endereços relativos em absolutos
+    // relativos -> ./src/index.html
+    // absolutos -> C:/Users/Usuario/Desktop/Projeto/src/index.html
 })
 
 app.get('/contato', (req, res) => {
-    res.send('<h1 style="color: blue; text-align: center;">Contatos</h1>')
+    res.sendFile(path.resolve('./src/contato.html'));
 })
 
-app.listen (port, () =>{
-    console.log (`Servidor rodando na porta: ${port}`)
-})  
+app.get('/servico', (req, res) => {
+    res.sendFile(path.resolve('./src/servico.html'));
+})
+
+app.listen(8080, () =>{
+    console.log('Servidor rodando na porta 8080')
+})
